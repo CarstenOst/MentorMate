0af6e15367f996412073c6df1433304d1aa2bb73
Carsten Østergaard <carsteno@uia.no>
2023-10-18 11:20:39 +0200

    Contemplating removing managers all together

--- Core/Managers/UserManager.php
+++ Core/Managers/UserManager.php
@@ -6,13 +6,8 @@ include_once '../../Infrastructure/Database/DBConnector.php';
 use Repositories\UserRepository;
 use Database\DBConnector;
 
+
 // TODO THIS IS ONLY TO CHECK THE DATABASE, AND IS NOT GOING TO BE LIKE THIS
-$database = new DBConnector();
-$db = $database->getConnection();
 
-$request = new UserRepository();
-$user = $request->read($db, 1);
 
-echo "User Type: " . $user->getUserType();
-echo "User Name: " . $user->getFirstName();
-echo "User Mail: " . $user->getEmail();
+
