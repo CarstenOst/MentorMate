6ea6c5f6f74ea6a649805230d5baa62f9f12ec32
Carsten Østergaard <carsteno@uia.no>
2023-10-18 11:21:50 +0200

    DBConnector.php now uses constant values for database connection

--- Infrastructure/Database/DBConnector.php
+++ Infrastructure/Database/DBConnector.php
@@ -1,22 +1,30 @@
 <?php
 
-namespace Database;
-include_once 'DBConfig.php'; // This file is not included on GitHub for security reasons. Make one yourself
+namespace Infrastructure\Database;
+// This file is not included on GitHub for security reasons. Make one yourself
 // with DB_HOST_NAME, DB_NAME and DB_PASSWORD constants
 // Note: you might have to add another constant for the username, as our db has same name and username for simplicity
 
-use PDOException;
 use PDO;
+use PDOException;
 
 class DBConnector
 {
+    /**
+     * This static method returns a PHP Data Object (PDO) connection to the database.
+     * Note, remember to add DBConfig.php with connection constants.
+     *
+     * @return PDO|null Returns a PDO connection to the database, or null if an exception is thrown.
+     * @throws PDOException Throws a PDOException if the connection fails.
+     *
+     */
     public static function getConnection(): ?PDO
     {
         try {
             $conn = new PDO(
-                "mysql:host=" . DB_HOST_NAME . ";dbname=" . DB_NAME,
-                DB_NAME,
-                DB_PASSWORD);
+                "mysql:host=" . DBConfig::DB_HOST_NAME . ";dbname=" . DBConfig::DB_NAME,
+                DBConfig::DB_NAME,
+                DBConfig::DB_PASSWORD);
 
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $conn->exec("set names utf8");
