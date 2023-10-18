02de0bb92ef4cabdc8ca01b86fdd32062b87ca13
Carsten Østergaard <carsteno@uia.no>
2023-10-18 11:20:10 +0200

    Controller now checks output

--- Application/Controllers/UserController.php
+++ Application/Controllers/UserController.php
@@ -1,6 +1,28 @@
 <?php
+require '../../autoloader.php';
+
+use Infrastructure\Repositories\UserRepository;
 
 class UserController
 {
 
+    public static function showUser($id): void
+    {
+        $user = UserRepository::read($id);
+        if (is_string($user)) {
+            echo $user;
+            return;
+        }
+        echo "User Type: " . $user->getUserType();
+        echo "<br>User Name: " . $user->getFirstName();
+        echo "<br>User Last Name: " . $user->getLastName();
+        echo "<br>User Mail: " . $user->getEmail();
+        echo "<br>User About: " . $user->getAbout();
+        echo "<br>User Created At: " . $user->getCreatedAt()->format('Y-m-d H:i:s');
+        echo "<br>User Updated At: " . $user->getUpdatedAt()->format('Y-m-d H:i:s');
+        echo "<br>User Password: " . $user->getPassword();
+    }
+
 }
+
+UserController::showUser(1);
