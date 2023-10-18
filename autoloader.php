ca21865887d9f580121e76c34a56f12515f29ff2
Carsten Østergaard <carsteno@uia.no>
2023-10-18 11:18:55 +0200

    Added autoloader.php and compose.json

---
+++ autoloader.php
@@ -0,0 +1,16 @@
+<?php
+
+
+spl_autoload_register(function ($fullyQualifiedClassName) {
+    // Base directory
+    $baseDir = __DIR__ ;  // Adjust this to your actual base directory
+
+    // Convert namespace to full file path
+    $path = $baseDir . '/' . str_replace('\\', '/', $fullyQualifiedClassName) . '.php';
+    if (file_exists($path)) {
+        require $path;
+    } else {
+        echo "File not found: $path";  // TODO Remove before flight
+    }
+});
+
