b12654b1a5406bb9578f2210bde533be6d312907
williamaredal <williamaredal@hotmail.com>
2023-10-26 20:36:30 +0200

    Now Profile also fetches user for display

--- Application/Views/User/Profile.php
+++ Application/Views/User/Profile.php
@@ -4,10 +4,31 @@ namespace Application\Views\User;
 
 require ("../../../autoloader.php");
 use Application\Views\Shared\Layout;
+use Core\Entities\User;
+use Infrastructure\Repositories\UserRepository;
 
 class Profile
 {
-
+    public static function viewUserProfile(int $userId) {
+        $user = UserRepository::read($userId);
+        if (is_string($user)) {
+            echo $user; // TODO call some common error msg display
+            return;
+        }
+        $firstName = $user->getFirstName();
+        $lastName = $user->getLastName();
+        $userType = $user->getUserType();
+        $email = $user->getEmail();
+        $about = $user->getAbout();
+        echo "
+            <div class='user-profile'>
+                <h5>$firstName $lastName</h5>
+                <p><small>$userType</small></p>
+                <p>Email: $email</p>
+                <p>Bio: $about</p>
+            </div>
+        ";
+    }
 }
 
 ?>
@@ -16,5 +37,6 @@ class Profile
 <html>
     <?php
         Layout::displayTop();
+        Profile::viewUserProfile(1);
     ?>
 </html>
