5f91b818835e1e7398b70c962504377969d68960
williamaredal <williamaredal@hotmail.com>
2023-11-23 12:52:11 +0100

    Updated BookingRepository interface

--- Core/Interfaces/IBookingRepository.php
+++ Core/Interfaces/IBookingRepository.php
@@ -2,7 +2,12 @@
 
 namespace Interfaces;
 
+use Core\Entities\Booking;
+
 interface IBookingRepository
 {
-
+    public static function create(Booking $user);
+    public static function read($id);
+    public static function update($booking);
+    public static function delete($id);
 }
