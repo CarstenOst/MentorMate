524abe36b7d243f41f233945109e281f14d564ad
williamaredal <williamaredal@hotmail.com>
2023-11-27 16:05:12 +0100

    Landing page and "logo" page

--- Application/Views/index.php
+++ Application/Views/index.php
@@ -8,25 +8,27 @@ use Application\Views\Shared\Layout;
 require("../../autoloader.php");
 
 
-// TODO add welcome page, with login page and link to register below
-Layout::displayTop();
 ?>
-<body>
-    <div>
-        <h2>MentorMate</h2>
-        <div>
-            <a style='text-decoration:none' href='./User/Login.php'>
-                <div class='submit-button'>
-                    Login
-                </div>
-            </a>
-            <a style='text-decoration:none' href='./User/Register.php'>
-                <div class='submit-button'>
-                    Register
-                </div>
-            </a>
-        </div>
+<head>
+    <link rel="stylesheet" href="/Assets/style.css">
+    <script src="https://kit.fontawesome.com/5928831ae4.js" crossorigin="anonymous"></script>
+</head>
 
+<body>
+
+    <div class='custom-container'>
+        <div>
+            <div>
+                <h2>MentorMate</h2>
+                <br>
+                <h4>What do you want to do?</h4>
+                <br>
+                <a href='../Views/Book/index.php' class='custom-img-link'><img width='160' height='160' src='../Assets/book.svg' alt='Book session'></a>
+                <a href='../Views/Bookings/index.php' class='custom-img-link'><img width='160' height='160' src='../Assets/bookings.svg' alt='Your booked sessions'></a>
+                <a href='../Views/Messages/index.php' class='custom-img-link'><img width='160' height='160' src='../Assets/messages.svg' alt='Your messages'></a>
+                <a href='../Views/User/Profile.php' class='custom-img-link'><img width='160' height='160' src='../Assets/profile.svg' alt='Your profile'></a>
+            </div>
+        </div>
     </div>
 
 </body>
