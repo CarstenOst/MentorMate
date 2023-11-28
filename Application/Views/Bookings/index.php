012c77d69bb464bf663cf6e99763d69dc7c98bb1
williamaredal <williamaredal@hotmail.com>
2023-11-28 12:52:06 +0100

    icon change calendar plus

--- Application/Views/Bookings/index.php
+++ Application/Views/Bookings/index.php
@@ -157,7 +157,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['book'])) {
                                     <button class='table-button' onclick='messageTutor($tutorId)'><i class='message-icon fa-solid fa-message'></i> Message</button>
                                 </td>
                                 <td>
-                                    <button class='table-button'><i class='calendar-icon fa-regular fa-calendar'></i> Add boooking</button>
+                                    <button class='table-button'><i class='calendar-icon fa-regular fa-calendar-plus'></i> Add boooking</button>
                                 </td>
                             </tr>
                         ";
