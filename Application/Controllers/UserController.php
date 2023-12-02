58b5b3901a4a6db6642dae1c0dd459a089717c3e
williamaredal <williamaredal@hotmail.com>
2023-12-02 16:09:32 +0100

    Layout for the UserController actions

--- Application/Controllers/UserController.php
+++ Application/Controllers/UserController.php
@@ -1,39 +1,30 @@
 <?php
-namespace Application\Controllers;
-require '../../autoloader.php';
 
+namespace Application\Controllers\UserController;
+
+require("../../autoloader.php");
+
+use Core\Entities\User;
+use DateTime;
 use Exception;
-use Infrastructure\Repositories\UserRepository;
-
-class UserController
-{
+use Application\Constants\SessionConst;
+use Application\Validators\Auth;
 
 
-    /**
-     * Controller to show the user.
-     *
-     * @param int $id Insert the userid to search for as int.
-     * @return void TODO change to return ok status or something.
-     * @throws Exception if datetime is not correctly set in the database.
-     */
-    public static function showUser(int $id): void
-    {
-        $user = UserRepository::read($id);
-        if (is_string($user)) {
-            echo $user; // TODO call some common error msg display
-            return;
-        }
-        include_once '../Views/User/Login.php';
-        echo "User Type: " . $user->getUserType();
-        echo "<br>User Name: " . $user->getFirstName();
-        echo "<br>User Last Name: " . $user->getLastName();
-        echo "<br>User Mail: " . $user->getEmail();
-        echo "<br>User About: " . $user->getAbout();
-        echo "<br>User Created At: " . $user->getCreatedAt()->format('Y-m-d H:i:s');
-        echo "<br>User Updated At: " . $user->getUpdatedAt()->format('Y-m-d H:i:s');
-        echo "<br>User Password: " . $user->getPassword();
-    }
-
+// Starts session, and checks if user is logged in. If not, redirects to login page
+if (!Auth::checkAuth()) {
+    header('Location: ../User/Login.php');
+    exit();
 }
 
-UserController::showUser(1);
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['action'])) {
+    switch ($_POST['action']) {
+        // Actions for
+        case 'case1':
+            break;
+
+    }
+
+    exit();
+}
