d8c487df863f2640690e02b707d2feb86adcb20f
Carsten Østergaard <carsteno@uia.no>
2023-12-05 20:58:07 +0100

    UserController.php now handles about, and picture update

--- Application/Controllers/UserController.php
+++ Application/Controllers/UserController.php
@@ -4,11 +4,11 @@ namespace Application\Controllers\UserController;
 
 require("../../autoloader.php");
 
-use Core\Entities\User;
-use DateTime;
-use Exception;
 use Application\Constants\SessionConst;
 use Application\Validators\Auth;
+use Application\Functions\ProfileImageHandler;
+use Application\Validators\Validator;
+use Infrastructure\Repositories\UserRepository;
 
 
 // Starts session, and checks if user is logged in. If not, redirects to login page
@@ -16,15 +16,42 @@ if (!Auth::checkAuth()) {
     header('Location: ../User/Login.php');
     exit();
 }
+header('Content-Type: application/json');
 
+$response = ['status' => 'error', 'message' => ''];
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['action'])) {
     switch ($_POST['action']) {
-        // Actions for
-        case 'case1':
+        case 'uploadProfilePicture':
+            $message = [];
+            if (ProfileImageHandler::uploadImage($_FILES['image'], $_SESSION[SessionConst::USER_ID], $message)) {
+                // If the upload is successful
+                $response = ['status' => 'success', 'message' => 'File uploaded successfully.'];
+            } else {
+                // Only set this response if the upload failed
+                $response = ['status' => 'error', 'message' => implode('<br>', $message)];
+            }
             break;
 
-    }
+        case 'updateAbout':
+            if (!Validator::isValid(Validator::STRIPPED_TEXT, $_POST['about'])){
+                $response = ['status' => 'error', 'message' => 'Not a valid text!'];
+                break;
+            }
+            $aboutText = $_POST['about']; // This should be clean at this point
 
-    exit();
+            // Update the about section in the database
+            $updateResult = UserRepository::updateAbout($_SESSION[SessionConst::USER_ID], $aboutText);
+
+            // If it is successful, update the session variable, and set a success message
+            if ($updateResult) {
+                $response = ['status' => 'success', 'message' => 'About section updated successfully.'];
+                $_SESSION[SessionConst::ABOUT] = $aboutText;
+            } else {
+                $response = ['status' => 'error', 'message' => 'Failed to update about section.'];
+            }
+            break;
+    }
+    // Send the response to the client (this is captured in the ajax call)
+    echo json_encode($response);
 }
