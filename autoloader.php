44fc472433d70b51d2ea48f3389c481afd99a865
Carsten Østergaard <carsteno@uia.no>
2023-12-05 09:52:09 +0100

    yuh

--- autoloader.php
+++ autoloader.php
@@ -3,7 +3,7 @@
 
 spl_autoload_register(function ($fullyQualifiedClassName) {
     // Base directory
-    $baseDir = __DIR__ ;  // Adjust this to your actual base directory
+    $baseDir = __DIR__ ;
 
     // Convert namespace to full file path
     $path = $baseDir . '/' . str_replace('\\', '/', $fullyQualifiedClassName) . '.php';
@@ -11,6 +11,7 @@ spl_autoload_register(function ($fullyQualifiedClassName) {
     if (file_exists($path)) {
         require $path;
     } else {
+        // TODO Silently make composer autoload it
         echo "File not found: $path";  // TODO Remove before flight
     }
 });
